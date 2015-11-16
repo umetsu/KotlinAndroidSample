6dcf97b609e0d062609c73a902723191e8408660
Umetsu Kentaro <umetsu.kentaro@adways.net>
2015-11-16 15:57:44 +0900

    Data Bindingを有効化。

--- app/src/main/kotlin/net/prunusmume/kotlinsample/MainActivity.kt
+++ app/src/main/kotlin/net/prunusmume/kotlinsample/MainActivity.kt
@@ -1,12 +1,15 @@
 package net.prunusmume.kotlinsample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import net.prunusmume.kotlinsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.textView.text = "Hello World!!"
     }
 }
